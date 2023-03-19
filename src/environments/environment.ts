dfb6a363c5be986120ed871f560a5463eabfff07
unknown <seydina.laye85@gmail.com>
2023-03-20 00:02:09 +0100

    Auth commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  firebase: {
+      apiKey: "AIzaSyBx4CaNFeWqTvObxbwBkAciNgQ9aaNMWCc",
+      authDomain: "sportdb-7ed6f.firebaseapp.com",
+      databaseURL: "https://sportdb-7ed6f-default-rtdb.europe-west1.firebasedatabase.app",
+      projectId: "sportdb-7ed6f",
+      storageBucket: "sportdb-7ed6f.appspot.com",
+      messagingSenderId: "371322936820",
+      appId: "1:371322936820:web:e91e3ad0aba09f598d6da1",
+      measurementId: "G-MQEBZN4SF3"
+  }
+};
